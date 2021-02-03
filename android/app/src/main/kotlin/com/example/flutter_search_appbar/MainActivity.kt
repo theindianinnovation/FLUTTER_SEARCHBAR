b1ab155eb9a36c47ccc01e6bb226775cb68d44a3
theindianinnovation <codestudio0110@gmail.com>
2021-02-03 13:03:10 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_search_appbar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_search_appbar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homescreen.dart
@@ -0,0 +1,171 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+//Step 3
+  _HomeScreenState() {
+    _filter.addListener(() {
+      if (_filter.text.isEmpty) {
+        setState(() {
+          _searchText = "";
+          filteredNames = names;
+        });
+      } else {
+        setState(() {
+          _searchText = _filter.text;
+        });
+      }
+    });
+  }
+
+//Step 1
+  final TextEditingController _filter = new TextEditingController();
+  final dio = new Dio(); // for http requests
+  String _searchText = "";
+  List names = new List(); // names we get from API
+  List filteredNames = new List(); // names filtered by search text
+  Icon _searchIcon = new Icon(Icons.search);
+  Widget _appBarTitle = new Text('Search Example');
+
+  //step 2.1
+  void _getNames() async {
+    final response =
+        await dio.get('https://jsonplaceholder.typicode.com/users');
+    print(response.data);
+    List tempList = new List();
+    for (int i = 0; i < response.data.length; i++) {
+      tempList.add(response.data[i]);
+    }
+    setState(() {
+      names = tempList;
+      filteredNames = names;
+    });
+  }
+
+//Step 2.2
+  void _searchPressed() {
+    setState(() {
+      if (this._searchIcon.icon == Icons.search) {
+        this._searchIcon = new Icon(Icons.close);
+        this._appBarTitle = new TextField(
+          controller: _filter,
+          decoration: new InputDecoration(
+              prefixIcon: new Icon(Icons.search), hintText: 'Search...'),
+        );
+      } else {
+        this._searchIcon = new Icon(Icons.search);
+        this._appBarTitle = new Text('Search Example');
+        filteredNames = names;
+        _filter.clear();
+      }
+    });
+  }
+
+  //Step 4
+  Widget _buildList() {
+    if (!(_searchText.isEmpty)) {
+      List tempList = new List();
+      for (int i = 0; i < filteredNames.length; i++) {
+        if (filteredNames[i]['name']
+            .toLowerCase()
+            .contains(_searchText.toLowerCase())) {
+          tempList.add(filteredNames[i]);
+        }
+      }
+      filteredNames = tempList;
+    }
+    return ListView.builder(
+      itemCount: names == null ? 0 : filteredNames.length,
+      itemBuilder: (BuildContext context, int index) {
+        return new ListTile(
+          title: Text(filteredNames[index]['name']),
+          onTap: () => print(filteredNames[index]['name']),
+        );
+      },
+    );
+  }
+
+  //STep6
+  Widget _buildBar(BuildContext context) {
+    return new AppBar(
+      centerTitle: true,
+      title: _appBarTitle,
+      leading: new IconButton(
+        icon: _searchIcon,
+        onPressed: _searchPressed,
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    _getNames();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildBar(context),
+      body: Container(
+        child: _buildList(),
+      ),
+      resizeToAvoidBottomPadding: false,
+//      floatingActionButton: FloatingActionButton(
+//        onPressed: _postName,
+//        child: Icon(Icons.add),
+//      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//void _postName() async {
+//  var data = {
+//    "id": 11,
+//    "name": "Romeo Graham",
+//    "username": "Bret",
+//    "email": "Sincere@april.biz",
+//    "address": {
+//      "street": "Kulas Light",
+//      "suite": "Apt. 556",
+//      "city": "Gwenborough",
+//      "zipcode": "92998-3874",
+//      "geo": {"lat": "-37.3159", "lng": "81.1496"}
+//    },
+//    "phone": "1-770-736-8031 x56442",
+//    "website": "hildegard.org",
+//    "company": {
+//      "name": "Romaguera-Crona",
+//      "catchPhrase": "Multi-layered client-server neural-net",
+//      "bs": "harness real-time e-markets"
+//    }
+//  };
+//  final info = await dio
+//      .post('https://jsonplaceholder.typicode.com/users', data: data)
+//      .then((value) => debugPrint(value.toString()));
+//}
